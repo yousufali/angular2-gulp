5bd8e4bd5d8d73e73497d3e5b72c1a7f506fc0ca
yousuf <mail.yousufali@gmail.com>
2016-02-21 23:39:04 -0600

    Initial Checkin

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+const gulp = require('gulp');
+const del = require('del');
+const tscConfig = require('./tsconfig.json');
+const sourcemaps = require('gulp-sourcemaps');
+const typescript = require('gulp-typescript');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+
+gulp.task('clean', function () {
+  return del('dist/**/*');
+});
+
+
+gulp.task('compile', ['clean'], function () {
+  return gulp
+    .src(tscConfig.files)
+    .pipe(sourcemaps.init())
+    .pipe(typescript(tscConfig.compilerOptions))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/app'));
+});
+
+gulp.task('copy:assets', ['clean'], function() {
+  return gulp.src(['app/**/*', 'index.html', 'styles.css', '!app/**/*.ts'], { base : './' })
+    .pipe(gulp.dest('dist'))
+});
+
+
+gulp.task('copy:libs', ['clean'], function() {
+  return gulp.src([
+      'node_modules/angular2/bundles/angular2-polyfills.js',
+      'node_modules/systemjs/dist/system.src.js',
+      'node_modules/rxjs/bundles/Rx.js',
+      'node_modules/angular2/bundles/angular2.dev.js',
+      'node_modules/angular2/bundles/router.dev.js',
+      'node_modules/node-uuid/uuid.js',
+      'node_modules/immutable/dist/immutable.js'
+    ])
+    .pipe(gulp.dest('dist/lib'))
+});
+
+gulp.task('compile', ['clean'], function () {
+  return gulp
+    .src(tscConfig.files)
+    .pipe(sourcemaps.init())
+    .pipe(typescript(tscConfig.compilerOptions))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/app'));
+});
+
+gulp.task('serve', ['build'], function() {
+  browserSync({
+    server: {
+      baseDir: 'dist'
+    }
+  });
+
+  gulp.watch(['app/**/*', 'index.html'], ['buildAndReload']);
+});
+
+gulp.task('build', ['compile', 'copy:libs', 'copy:assets']);
+gulp.task('buildAndReload', ['build'], reload);
+gulp.task('default', ['build']);
